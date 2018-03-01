f45f471178ab71bcf859f2a334817aabfb0648c6
mahesh gawande <maheshsg297@gmail.com>
2018-03-01 14:07:22 +0530

    dynamic webpage demo added

---
+++ demo.js
@@ -0,0 +1,25 @@
+function addDiv() {
+  var printDiv = document.createElement('div');
+  printDiv.className = "newDiv";
+  var childDiv = document.getElementById('dispDiv').appendChild(printDiv);
+  count = parseInt(document.getElementsByClassName('newDiv').length);
+  var divId = "div" + count;
+  var newDivId = printDiv.id = divId;
+
+  var divTxt = document.createElement('span');
+  divTxt.className = "txt";
+  divTxt.id = "span" + count;
+  divTxt.innerHTML = "Div " + count;
+  document.getElementById(newDivId).appendChild(divTxt);
+
+  var dltBtn = document.createElement('span');
+  dltBtn.className = "dltSpan";
+  dltBtn.id = "span" + count;
+  dltBtn.innerHTML = "X";
+  document.getElementById(newDivId).appendChild(dltBtn);
+  dltBtn.onclick = removeDiv;
+
+  function removeDiv() {
+    document.getElementById(divId).remove(childDiv);
+  };
+};
