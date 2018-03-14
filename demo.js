b7bf7124d51defe57e9f478a20f8fbadbc8631a4
mahesh gawande <maheshsg297@gmail.com>
2018-03-14 17:46:39 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,25 +1,49 @@
+var createArr = [];
+var deleteArr = [];
+
 function addDiv() {
-  var printDiv = document.createElement('div');
-  printDiv.className = "newDiv";
-  var childDiv = document.getElementById('dispDiv').appendChild(printDiv);
-  count = parseInt(document.getElementsByClassName('newDiv').length);
-  var divId = "div" + count;
-  var newDivId = printDiv.id = divId;
 
-  var divTxt = document.createElement('span');
-  divTxt.className = "txt";
-  divTxt.id = "span" + count;
-  divTxt.innerHTML = "Div " + count;
-  document.getElementById(newDivId).appendChild(divTxt);
+  if (deleteArr.length == "") {
+    if (createArr.length < 5) {
+      var printDiv = document.createElement('div');
+      printDiv.className = "newDiv";
+      var childDiv = document.getElementById('dispDiv').appendChild(printDiv);
+      count = parseInt(document.getElementsByClassName('newDiv').length);
+      var divId = "div" + count;
+      var newDivId = printDiv.id = divId;
 
-  var dltBtn = document.createElement('span');
-  dltBtn.className = "dltSpan";
-  dltBtn.id = "span" + count;
-  dltBtn.innerHTML = "X";
-  document.getElementById(newDivId).appendChild(dltBtn);
-  dltBtn.onclick = removeDiv;
+      var divTxt = document.createElement('span');
+      divTxt.className = "txt";
+      divTxt.id = "span" + count;
+      divTxt.innerHTML = "Div " + count;
+      document.getElementById(newDivId).appendChild(divTxt);
+
+      var dltBtn = document.createElement('span');
+      dltBtn.className = "dltSpan";
+      dltBtn.id = "span" + count;
+      dltBtn.innerHTML = "X";
+      document.getElementById(newDivId).appendChild(dltBtn);
+      dltBtn.onclick = removeDiv;
+
+      var count = parseInt(document.getElementsByClassName('newDiv').length);
+      createArr.push(count);
+    }else {
+      alert("Sorry, you cannot add another div!");
+    };
+  }else {
+
+      var poppedElement = deleteArr.pop();
+      var getElement = document.getElementById("div"+ poppedElement );
+      console.log(getElement);
+      //
+      // console.log(poppedElement);
+      // console.log(deleteArr);
+  };
 
   function removeDiv() {
-    document.getElementById(divId).remove(childDiv);
+    document.getElementById(divId).remove();
+    deltedDiv = createArr.pop(divId);
+    deleteArr.push(deltedDiv);
+    console.log(deleteArr);
   };
 };
